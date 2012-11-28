f098f3f634a80138786146c04f290280247786ba
Stefano Palazzo <stefano.palazzo@gmail.com>
2012-11-28 06:24:28 +0100

    permissions

--- precise/nodejs/server/server.js
+++ precise/nodejs/server/server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
+var express = require('express')
+var fs = require('fs')
+var app = express()
 var config = JSON.parse(fs.readFileSync("config.json"))
 
 
 app.get('/', function(req, res){
-  res.send(config["greeting"] || "Hello World");
-});
+  res.send(config["greeting"] || "Hello World")
+})
 
-app.listen(config["port"]);
-console.log('Listening on port 3000');
+app.listen(config["port"])
+console.log('Listening on port', config["port"])
